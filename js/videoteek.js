e5c62445a374f62c0f958520797241364cbe7332
Riko M <riko.murutalu@gmail.com>
2020-04-06 16:25:07 +0300

    videoteek valmis

--- js/videoteek.js
+++ js/videoteek.js
@@ -1,16 +1,17 @@
-function openTab(eventTarget, tabName) {
-    var i = tabcontent, tablinks;
+document.getElementsByClassName('tablinks')[0].click();
 
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+      tabcontent[i].style.display = "none";
     };
 
-    tablinks = document.getElementsByClassName("tablink");
+    tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
     };
-    
-    document.getElementById(tabName).style.display = "block";
-    eventTarget.currentTarget.className += " active";
-};
+
+    document.getElementById(tabName).style.display = "flex";
+    evt.currentTarget.className += " active";
+  };
