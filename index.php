62f4625bc537d634f7fc87828750094f32ab67a8
Riko M <riko.murutalu@gmail.com>
2020-04-06 11:02:47 +0300

    vahepealne korrastamine

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kogu Me Lugu</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css" />
     <link rel="stylesheet" href="./scss/styles.css">
     
 </head>
@@ -13,12 +12,24 @@
 <body>
     <?php include './header.html' ?>
 
+    <main>
+        <?php include './searchbar.html' ?>
+
+
+
+        <?php include './shareBtn.html' ?>
+    </main>
+    
+
 
 
     <?php include './footer.html' ?>
 </body>
 
 
-
+    <script src="js/header.js"></script>
+    <script src="js/modal.js"></script>
+    <script src="js/map.js"></script>
+    <script src="js/sharebtn.js"></script>
 
 </html>
---
+++ js/videoteek.js
@@ -0,0 +1,16 @@
+function openTab(eventTarget, tabName) {
+    var i = tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    };
+
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    };
+    
+    document.getElementById(tabName).style.display = "block";
+    eventTarget.currentTarget.className += " active";
+};
