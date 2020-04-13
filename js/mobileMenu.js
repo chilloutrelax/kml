a6e59c5d034f0a65793aedb7dd00a07b98371538
Riko M <riko.murutalu@gmail.com>
2020-04-13 15:48:38 +0300

    väike menu parandus

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -1,7 +1,15 @@
 function openNav() {
-    document.getElementById("mobileMenu").style.width = "200px";
-  }
+  document.getElementById("mobileMenu").style.width = "200px";
+};
   
-  function closeNav() {
-    document.getElementById("mobileMenu").style.width = "0";
-  }
+function closeNav() {
+  document.getElementById("mobileMenu").style.width = "0";
+}
+
+window.addEventListener('mouseup', function(event){
+  var menu = document.getElementById('mobileMenu');
+
+  if(event.target != menu) {
+    menu.style.width = "0";
+  };
+});
